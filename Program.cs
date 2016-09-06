5630c7a51f7d1361619a594622df68bb7e49a0cb
McKay Heares <rosanneheares@Rosannes-MacBook-Pro.local>
2016-09-06 22:05:16 +1000

    Added test syntax

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Linq;
 
 namespace ConsoleApplication
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			var x = new string[]{
+				"",
+				""
+			};
+
+			x.Select(z=>{
+				return "";
+			}).ToList();
+
+			
+		}
+	}
 }
