5314dab8934dc7f774aea5b200591af141e8900a
McKay Heares <rosanneheares@Rosannes-MacBook-Pro.local>
2016-09-09 22:52:40 +1000

    Removed unusings

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using dotapi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
