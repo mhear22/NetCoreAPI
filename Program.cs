7bc83c1f95ae4b3eb7f1035b2b7bd1f237733129
McKay Heares <rosanneheares@Rosannes-MacBook-Pro.local>
2016-09-09 18:55:12 +1000

    Added template content

--- Program.cs
+++ Program.cs
@@ -1,23 +1,32 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
-namespace ConsoleApplication
+namespace dotapi
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-			var x = new string[]{
-				"",
-				""
-			};
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+            
+            var host = new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
 
-			x.Select(z=>{
-				return "";
-			}).ToList();
-
-			
-		}
-	}
+            host.Run();
+        }
+    }
 }
