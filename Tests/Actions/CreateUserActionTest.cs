913653fcedb483a3714aadc04777456ee5c5a622
McKay Heares <rosanneheares@Rosannes-MacBook-Pro.local>
2016-09-17 18:34:08 +1000

    tests for controller endpoints

---
+++ Tests/Actions/CreateUserActionTest.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace dotapi.Tests.Actions
+{
+	public class CreateUserActionTest : ActionTestBase
+	{
+		
+		public CreateUserActionTest(){ }
+		
+		[Fact]
+		public void CreateSingleUser()
+		{
+			// /users 
+			//Assert.True(result != null);
+		}
+	}
+}
