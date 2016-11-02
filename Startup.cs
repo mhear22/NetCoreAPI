7bbd2f976d49b6646540b7f3f77f49d79812cc7f
McKay <mhear22@gmail.com>
2016-11-02 21:22:09 +1000

    Enabled Cors

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,13 @@ namespace dotapi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+			services.AddCors(x=>{
+				x.AddPolicy("cors", z=> z.AllowAnyOrigin()
+					.AllowAnyMethod()
+					.AllowAnyHeader()
+					.AllowCredentials()
+				);
+			});
 			services.AddDbContext<DatabaseContext>(options => options.UseInMemoryDatabase(),ServiceLifetime.Transient);
 			services.AddSingleton<IAuthenticationService, AuthenticationService>();
 			services.AddSingleton<IPasswordService, PasswordService>();
@@ -36,6 +43,7 @@ namespace dotapi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+			app.UseCors("cors");
             app.UseMvc();
         }
     }
