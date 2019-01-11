890698be22db3b47a616229d28c0415db4e523f1
McKay <mhear22@gmail.com>
2019-01-12 00:57:27 +1000

    front end can accept payments, dummy back end pretends

---
+++ CoreApp/Controllers/PaymentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreApp.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApp.Controllers
+{
+	public class PaymentController : ApiController
+	{
+		public PaymentController(IContext context)
+			: base(context)
+		{
+		}
+
+		[HttpPost]
+		[Route("payments/")]
+		[ProducesResponseType(200, Type=typeof(PaymentModel))]
+		public IActionResult ProcessPayment([FromBody]PaymentModel model) => ReturnResult(() =>
+		{
+			return model;
+		});
+	}
+
+	public class PaymentModel
+	{
+		public object Token;
+		public string Amount;
+	}
+}
