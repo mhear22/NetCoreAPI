09ba119e48837d9b96bc8dc182b1a7ff30745b3a
McKay <mhear22@gmail.com>
2019-01-22 14:48:51 +1000

    no app settings

--- CoreApp.Serverless/LambdaEntryPoint.cs
+++ CoreApp.Serverless/LambdaEntryPoint.cs
@@ -8,23 +8,23 @@ using System.IO;
 
 namespace CoreApp.Serverless
 {
-    /// <summary>
-    /// This class extends from APIGatewayProxyFunction which contains the method FunctionHandlerAsync which is the 
-    /// actual Lambda function entry point. The Lambda handler field should be set to
-    /// 
-    /// CoreApp.Serverless::CoreApp.Serverless.LambdaEntryPoint::FunctionHandlerAsync
-    /// </summary>
-    public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
-    {
-        /// <summary>
-        /// The builder has configuration, logging and Amazon API Gateway already configured. The startup class
-        /// needs to be configured in this method using the UseStartup<>() method.
-        /// </summary>
-        /// <param name="builder"></param>
-        protected override void Init(IWebHostBuilder builder)
-        {
-            builder
-                .UseStartup<CoreApp.Startup>();
-        }
-    }
+	/// <summary>
+	/// This class extends from APIGatewayProxyFunction which contains the method FunctionHandlerAsync which is the 
+	/// actual Lambda function entry point. The Lambda handler field should be set to
+	/// 
+	/// CoreApp.Serverless::CoreApp.Serverless.LambdaEntryPoint::FunctionHandlerAsync
+	/// </summary>
+	public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+	{
+		/// <summary>
+		/// The builder has configuration, logging and Amazon API Gateway already configured. The startup class
+		/// needs to be configured in this method using the UseStartup<>() method.
+		/// </summary>
+		/// <param name="builder"></param>
+		protected override void Init(IWebHostBuilder builder)
+		{
+			builder
+				.UseStartup<CoreApp.Startup>();
+		}
+	}
 }
